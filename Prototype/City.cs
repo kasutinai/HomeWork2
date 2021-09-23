a178ee010aa835a76ba47fc953db4853d9b201ac
kasutinai <ak9mail@mail.ru>
2021-09-23 15:17:22 +0700

    23.09.2021

---
+++ Prototype/City.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{//Класс города в справочнике адресов, верхний уровень адреса
+    class City : IMyCloneable<City>, ICloneable
+    {
+        public string CityName { get; set; }
+
+        public City(string cityName)
+        {
+            CityName = cityName;
+        }
+
+        public City MyClone()
+        {
+            return new City(CityName);
+        }
+
+        public override string ToString()
+        {
+            return CityName;
+        }
+
+        public object Clone()
+        {
+            return MyClone();
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            City city           = new City("MyCloneCity");
+            Street street       = new Street(city.CityName, "MyCloneStreet");
+            Building building   = new Building(street.CityName, street.StreetName, "MyCloneBuilding");
+
+            City city1          = city.MyClone();
+            Street street1      = street.MyClone();
+            Building building1  = building.MyClone();
+
+            ChangeAddress(city, "CloneCity", street, "CloneStreet", building, "CloneBuilding");            
+
+            City city2          = (City)city.Clone();
+            Street street2      = (Street) street.Clone();
+            Building building2  = (Building) building.Clone();
+
+            ChangeAddress(city, "BasicCity", street, "BasicStreet", building, "BasicBuilding");
+
+            Console.WriteLine($"Basic : {city.ToString()} {street.ToString()} {building.ToString()}");
+            Console.WriteLine($"MyClone : {city1.ToString()} {street1.ToString()} {building1.ToString()}");
+            Console.WriteLine($"Clone : {city2.ToString()} {street2.ToString()} {building2.ToString()}");
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeAddress(City city, string cityName, Street street, string streetName, Building building, string buildingNumber)
+        {
+            city.CityName = cityName;
+            street.CityName = cityName;
+            street.StreetName = streetName;
+            building.CityName = cityName;
+            building.StreetName = streetName;
+            building.BuildingNumber = buildingNumber;
+        }
+    }
+}
